f62ab2f8bae446f9ca51b4e1e3dd079530f6686a
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-04-23 22:18:53 -0500

    function continue lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+saturdayFun("meet my friends");
+
+
+ function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+ }
+
+ mondayWork();
+ mondayWork("work from my home office");
+
+
+ function wrapAdjective(wrap = '*') {
+    return function(result = "a hard worker" ){
+        return `You are ${wrap}${result}${wrap}!`;
+    };
+ }
+
+
+ function wrapAdjective(wrap = "||"){
+    return function(result = "a dedicated programmer"){
+        return `You are ${wrap}${result}${wrap}!`
+    }
+ }
